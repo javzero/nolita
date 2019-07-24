079a25647da9bd3e198b7283bb348991b869a2e2
javzero <javzero1@gmail.com>
2019-07-24 20:25:14 -0300

    Google Pixel added

--- resources/views/store/partials/main.blade.php
+++ resources/views/store/partials/main.blade.php
@@ -114,8 +114,27 @@
 				checkoutSidebar('show');
 			</script>
 		@endif
-<script type="text/javascript">var subscribersSiteId='7d6e1b8a-5c74-498bb968-a733183b514e';</script><script type="text/javascript" src="https://cdn.subscribers.com/assets/subscribers.js"></script>
-		
+			<!-- Facebook Pixel Code -->
+	<!-- Facebook Pixel Code -->
+	<!-- Facebook Pixel Code -->
+	<script>
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t,s)}(window, document,'script',
+		'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '2268507203269857');
+		fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=2268507203269857&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
+
+	<script type="text/javascript">var subscribersSiteId='7d6e1b8a-5c74-498bb968-a733183b514e';</script><script type="text/javascript" src="https://cdn.subscribers.com/assets/subscribers.js"></script>
 	</body>
 </html>
 
