d641761aa2e65f49e9c290202ec517409abf6d98
javzero <javzero1@gmail.com>
2019-07-29 18:23:17 -0300

    Google Tag Manager Script Added

--- resources/views/store/partials/main.blade.php
+++ resources/views/store/partials/main.blade.php
@@ -43,10 +43,20 @@
 		</script>
 		<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1002215936641276&ev=PageView&noscript=1"/></noscript>
 		<!-- End Facebook Pixel Code -->
-		
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WCPHT5V');</script>
+		<!-- End Google Tag Manager -->
 	</head>
 	<!-- Body-->
 	<body>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCPHT5V"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+			<!-- End Google Tag Manager (noscript) -->
 		<div class="main-wrapper">
 			{{-- <div id="MainOverlay" class="main-overlay"></div> --}}
 			<div id="full-loader" class="full-loader Hidden">
